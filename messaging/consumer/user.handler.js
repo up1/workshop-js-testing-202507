f01bbc295307f4391f5f7eaaccaad198ec7e49b0
up1 <somkiat.p@gmail.com>
2025-07-23 23:57:16 +0700

    Validate jsonschema

--- messaging/consumer/user.handler.js
+++ messaging/consumer/user.handler.js
@@ -1,8 +1,22 @@
+const zod = require("zod");
 const User = require('./user');
 const userRepository = require('./user.repository');
 
+const schema = zod.object({
+  id: zod.number(),
+  email: zod.email(),
+  name: zod.string(),
+  age: zod.number(),
+});
+
 const handler = (message) => {
   console.log("[x] Processing user event:", message);
+  // Validate jsonschema if needed
+  const result = schema.safeParse(message);
+  if (!result.success) {
+    return Promise.reject(new Error("Invalid user event"));
+  }
+
   return Promise.resolve(userRepository.insert(new User(message.id, message.name, message.email, message.age)));
 }
 
